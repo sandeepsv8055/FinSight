1bf765525c520ea3336a9853a9015cf4c58cd556
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-07-25 11:36:09 +0530

    .

--- finsight/middleware.js
+++ finsight/middleware.js
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction",
+])
 
 export default clerkMiddleware();
 

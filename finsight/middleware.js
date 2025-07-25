981f64b179a573533d187b15242d0f0767a40f8a
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-07-25 12:28:04 +0530

    .

--- finsight/middleware.js
+++ finsight/middleware.js
@@ -4,9 +4,17 @@ const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction",
-])
+]);
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth,req)=>{
+  const {userId } = await auth();
+
+  if(!userId && isProtectedRoute(req)){
+    const { redirectToSignIn} = await auth(); 
+
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
